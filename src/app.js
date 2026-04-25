2c50946ab441b91d6ed5b46efcc861af4d5a098f
Aparna Samudrala <samudrala.aparna89@gmail.com>
2026-04-25 20:57:40 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,22 +1,30 @@
 //console.log("Starting a new Project!");
 const express = require("express");
 const app = express(); //instance of express application
-app.use("/contact", (req, res) => {
-  res.send("Hello from the contact route!!");
+//This will match only GET API calls to /user route
+//If you use app.use first even before specifying the HTTP method, then it will match all the API calls to /user route  and always it only execute the first callback function and never execute the other callback functions for the same route
+//that means if you use app.use first for /user route, then it will always execute the callback function of app.use and never execute the callback functions of app.get, app.post and app.delete for /user route
+//This will match all the HTTP method API calls to /user route . Hence other http methods never get executed for /user route  so order matters in express application
+// app.use("/user", (req, res) => {
+//   res.send("Hello from the user route!!");
+// });
+app.get("/user", (req, res) => {
+  res.send({ firstName: "John", lastName: "Doe" });
 });
-app.use("/about", (req, res) => {
-  res.send("Hello from the about route!!");
+
+app.post("/user", (req, res) => {
+  console.log("Save data to the database!!");
+  res.send("Data successfully saved to the database!!");
 });
+
+app.delete("/user", (req, res) => {
+  console.log("Delete data from the database!!");
+  res.send("Data successfully deleted from the database!!");
+});
+//This will match all the HTTP method API calls to /test route
 app.use("/test", (req, res) => {
   res.send("Hello from the test route!!");
 });
-app.use("/", (req, res) => {
-  console.log("Request received!!");
-  res.send("Namaste from the dashboard!");
-});
-app.use((req, res) => {
-  res.send("Hello from the server!");
-}); //request handler
 
 app.listen(7777, () => {
   console.log("Server is successfully listeing on prt 7777...");
