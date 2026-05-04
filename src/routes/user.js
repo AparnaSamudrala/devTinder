cd9e821ae2d9ec386a2df4cb8df8490cb88adc86
Aparna Samudrala <samudrala.aparna89@gmail.com>
2026-05-05 01:07:57 +0530

    feed api and pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
-
+const User = require("../models/user");
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender bio skills";
 // Get all the  pending connection request for the logged in user.
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -56,4 +57,54 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    //User should see all the user card
+    //Not his own card
+    // Not his connections (meaning he got it accepted)
+    // Not ignored people
+    // already sent the connection request
+    // Example : Rahul : [Mark, Donald, MS Dhoni, Virat]
+    // R -> Akshay and Akshay rejected,
+    //Rahul -> Elon -> Accepted
+    // Elon = [All except (Rauhl and himself)]
+    //Akshay = [All except (Rahul and himself)]
+    //Everyone can see new feeds which they have never seen before
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    //Find all the connection requests that logged in user has sent and received
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+        },
+        { toUserId: loggedInUser._id },
+      ],
+    })
+      .select("fromUserId toUserId status")
+      .populate("fromUserId", "firstName")
+      .populate("toUserId", "firstName");
+    //Set is an array that ignores duplicate items in that array
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId._id.toString());
+      hideUsersFromFeed.add(req.toUserId._id.toString());
+    });
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+    res.json({ length: users.length, data: users });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 module.exports = userRouter;
